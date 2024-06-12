0885cc82c5cb2ad8727ffbde5a8a305fafe6a33f
Nathan Arthur <nathan@nathanarthur.com>
2024-06-12 09:22:02 -0400

    add portfolio collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,5 @@
 // 1. Import utilities from `astro:content`
-import { z, defineCollection } from 'astro:content';
+import { z, defineCollection } from "astro:content";
 
 // 2. Define your collection(s)
 const blogCollection = defineCollection({
@@ -11,8 +11,8 @@ const blogCollection = defineCollection({
       src: z.string(),
       alt: z.string(),
     }),
-    publishDate: z.string().transform(str => new Date(str)),
-    author: z.string().default('Astroship'),
+    publishDate: z.string().transform((str) => new Date(str)),
+    author: z.string().default("Astroship"),
     category: z.string(),
     tags: z.array(z.string()),
   }),
@@ -27,13 +27,29 @@ const teamCollection = defineCollection({
       src: z.string(),
       alt: z.string(),
     }),
-    publishDate: z.string().transform(str => new Date(str)),
+    publishDate: z.string().transform((str) => new Date(str)),
+  }),
+});
+
+const portfolioCollection = defineCollection({
+  schema: z.object({
+    draft: z.boolean(),
+    title: z.string(),
+    description: z.string(),
+    image: z.object({
+      src: z.string(),
+      alt: z.string(),
+    }),
+    publishDate: z.string().transform((str) => new Date(str)),
+    category: z.string(),
+    tags: z.array(z.string()),
   }),
 });
 
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
-  'blog': blogCollection,
-  'team': teamCollection,
+  blog: blogCollection,
+  team: teamCollection,
+  portfolio: portfolioCollection,
 };
